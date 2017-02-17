acf5535981645da307be261156cf86d4fd592c0e
zgs225 <zgs225@gmail.com>
2017-02-17 09:19:57 +0800

    完善词法解析器

--- src/list_parser_test.c
+++ src/list_parser_test.c
@@ -1,24 +1,48 @@
-#include "list_parser.h"
 #include <stdio.h>
+#include "basic.h"
 
 
 int main(int argc, const char *argv[])
 {
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage: ./list_parser '[a, b, [c, d]]'\n");
         return 1;
     }
 
     Input *input;
+    Token *token;
 
-    if (LEXER_FN(init_input)(argv[1], &input) < 0) {
+    if (init_input(argv[1], &input) < 0)
+    {
         printf("Input init error\n");
         return 1;
     }
 
-    printf("Input: %s, length: %zu, current position: %d\n", input->input, input->len, input->pos);
+    while(1)
+    {
+        if (next_token(input, &token) < 0)
+        {
+            printf("next_token error\n");
+            return 1;
+        }
+        printf("%s\n", repr_token(token));
 
-    LEXER_FN(free_input)(input);
+        if (token->type == T_EOF)
+            break;
+
+        free_token(token);
+        token = NULL;
+    }
+
+    if (token)
+    {
+        free_token(token);
+        token = NULL;
+    }
+
+    free_input(input);
+    input = NULL;
 
     return 0;
 }
